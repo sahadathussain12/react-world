57ba587f9553108e6376e75586152f1fda3df0c4
sahadathussain12 <sahadathussain872856@gmail.com>
2026-04-12 12:05:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Country from './Country'
+
+function App() {
+ 
+
+  return (
+    <>
+        <h1>The React world on the go </h1>
+        <Country></Country>
+
+    </>
+  )
+}
+
+export default App
